1940451aed75b9aac54b3944b4882d44fcfb8d6a
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-28 18:41:30 -0500

    [ADD] files.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev',
+        permanent: true,
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
