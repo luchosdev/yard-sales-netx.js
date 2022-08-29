c7f04a00ff0257de87de22f73c0e19017926765f
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-29 15:02:26 -0500

    [FIX] scripts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <title>React Shop</title>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PCMF525D7D"></script>
-                    <script dangerouslySetInnerHTML={
-                        __html: `window.dataLayer = window.dataLayer || [];
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PCMF525D7D"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                        gtag('config', 'G-PCMF525D7D');`
-                    } />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+                        gtag('config', 'G-PCMF525D7D');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
