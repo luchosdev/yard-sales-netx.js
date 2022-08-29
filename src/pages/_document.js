d29fc8509d8c67b5ceecc8a0634a4bc9d3f8c5b0
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-29 13:12:20 -0500

    [ADD] configuration with google analytics in _document.js file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
@@ -6,12 +6,21 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <title>React Shop</title>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PCMF525D7D"></script>
+                    <script dangerouslySetInnerHTML={
+                        __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-PCMF525D7D');`
+                    } />
                 </Head>
-                <Body>
+                <body>
                     <Main />
                     <NextScript />
-                </Body>
+                </body>
             </Html>
-        )
+        );
     }
 }
+
+export default MyDocument;
