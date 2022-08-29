87d2e577f87d6153a3e24697bc4c5e0b8ca73fc0
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-29 15:11:03 -0500

    [FIX] scripts google analytics in _app.js and _document.js files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,23 @@
-import '@styles/globals.css';
 import Header from '@components/Header';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PCMF525D7D" />
+      <Script id="google-analytics" strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-PCMF525D7D');
+        `}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
