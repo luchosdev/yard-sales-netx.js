a8d896017cfacf3c4c2d9c79af137811451896f7
Luis Sequera <luissequeracontreras@gmail.com>
2022-08-29 20:46:05 -0500

    [ADD] configuration to PWA

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-};
-/*   env: {
-    customKey: 'customValue',
-  },
-  basePath: '/dist',
-  compress: true,
-  async redirects() {
-    return [
-      {
-        source: '/hola',
-        destination: 'https://gndx.dev',
-        permanent: true,
-      }
-    ]
-  } */
+});
